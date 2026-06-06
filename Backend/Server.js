b9d92d7b79063babd2fe95b63a65616ae22a731b
Vansh8788 <Vanshmalhotra24053@gmail.com/>
2026-06-06 13:56:05 +0530

    weather app

---
+++ Backend/Server.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+app.get("/weather/:city", async (req, res) => {
+    try {
+        const city = req.params.city;
+
+        const response = await axios.get(
+            `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.WEATHER_API_KEY}&units=metric`
+        );
+
+        res.json(response.data);
+
+    } catch (error) {
+        res.status(500).json({
+            message: "City not found"
+        });
+    }
+});
+
+app.listen(process.env.PORT, () => {
+    console.log("Server running");
+});
---
+++ frontend/frontend/src/App.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import axios from "axios";
+
+function App() {
+
+  const [city, setCity] = useState("");
+  const [weather, setWeather] = useState(false);
+
+  const getWeather = async () => {
+
+    const res = await axios.get(
+      `http://localhost:5000/weather/${city}`
+    );
+
+    setWeather(res.data);
+  };
+
+  return (
+    <div className="container">
+
+      <h1>Weather Forecast</h1>
+
+      <input
+        type="text"
+        placeholder="Enter city"
+        onChange={(e) => setCity(e.target.value)}
+      />
+
+      <button onClick={getWeather}>
+        Search
+      </button>
+
+      {weather && (
+        <div>
+          <h2>{weather.name}</h2>
+          <h3>{weather.main.temp}°C</h3>
+          <p>{weather.weather[0].description}</p>
+          <p>Humidity: {weather.main.humidity}%</p>
+          <p>Wind: {weather.wind.speed} km/h</p>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
